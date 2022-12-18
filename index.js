53ad640fe65e91ed5394d5b7980555cd3b4cb326
thomas <tom.angotti11@gmail.com>
2022-12-17 19:45:54 -0700

    commit tree 11511f9be18727dbe3661d030e507007a4953fe2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, unserEmail) {
+    const fetchInfo = {
+        
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: unserEmail,
+            })
+    }
+    fetch("http://localhost:3000/users", fetchInfo)
+    .then(Response => Response.json())
+    .then(data => console.log(data))
+    .catch(function (error) {
+        alert("RUHRO!");
+        console.log(error.message)
+    })
+    return fetch
+}
+
+submitData('Thomas', 'tom@gmail.com')
